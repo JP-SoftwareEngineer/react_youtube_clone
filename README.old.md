1725488665584007b6dbe3f64e907b7ffdc04f57
JPEngine <72838583+JPEngine@users.noreply.github.com>
2023-12-06 12:02:33 -0500

    a YouTube React Clone, to be completed later

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react_youtube_clone
+A clone of the YouTube web app using React JavaScript
---
+++ src/components/Videos.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Videos = () => (
+
+    <div>Videos</div>
+);
+
+export default Videos;
